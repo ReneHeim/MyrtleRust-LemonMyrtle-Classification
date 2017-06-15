2892cae82950e617b84fea8a3b4dcd86590a2fec
Rene Heim <rene.heim@hdr.mq.edu.au>
2017-06-15 11:36:44 +1000

    Final script now

--- MyrtleRust-LemonMyrtle-Classification FINAL script.R
+++ MyrtleRust-LemonMyrtle-Classification FINAL script.R
@@ -38,7 +38,7 @@ data.original <- read.csv('Input_for_C1_AllSpectraABGPlantation_LeafClip.csv', a
 ####
 
 # 2.1 Removing noisy ends from electromagnetic spectrum (500:2500)
-start <- match('501', names(data.original)) #Find start wavelength
+start <- match('500', names(data.original)) #Find start wavelength
 end <- match('2500', names(data.original)) #Find end wavelength
 
 data.rmv.noise <- data.original[,start:end] #to find correct bands according to col name use match('500',names(x))
